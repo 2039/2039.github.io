f373faa9ef3ff5ccb513b7590b8feb8d36b412b7
Hauk-Morten <haukmj@stud.ntnu.no>
2019-11-05 09:31:54 +0100

    Updated styles

--- webpage/script/translator.js
+++ webpage/script/translator.js
@@ -1,5 +1,3 @@
-
-
 const inText = document.getElementById('input');
 
 const vowels = "aeiouy";
@@ -8,29 +6,28 @@ const punctutation = ".,?!;:"
 const startsWithVowel = (word) => vowels.includes(word[0]);
 
 const endsWithPunctuation = (word) => {
-  return punctutation.includes(word.slice(-1))
+    return punctutation.includes(word.slice(-1))
 }
 
 const translateWord = (word) => {
-  let punctuationEnd="";
-  let validWord = word;
+    let punctuationEnd = "";
+    let validWord = word;
 
-  if (endsWithPunctuation(word)){
-    punctuationEnd = word.slice(-1);
-    validWord = word.slice(0,-1);
-  }
-  if (startsWithVowel(validWord)) {
-    var newWord = validWord + "'ay" + punctuationEnd;
+    if (endsWithPunctuation(word)) {
+        punctuationEnd = word.slice(-1);
+        validWord = word.slice(0, -1);
+    }
+    if (startsWithVowel(validWord)) {
+        var newWord = validWord + "'ay" + punctuationEnd;
 
-  }
-  else {
-    var newWord  = validWord.slice(1) + validWord.charAt(0) + "ay" + punctuationEnd;
-  }
-  // Correct capitalization if messed up
-  if (newWord != newWord.toLowerCase()){
-    newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1).toLowerCase();
-  }
-  return newWord;
+    } else {
+        var newWord = validWord.slice(1) + validWord.charAt(0) + "ay" + punctuationEnd;
+    }
+    // Correct capitalization if messed up
+    if (newWord != newWord.toLowerCase()) {
+        newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1).toLowerCase();
+    }
+    return newWord;
 }
 
 
@@ -39,10 +36,10 @@ const translateWord = (word) => {
 //
 // const translatedText = translatedArray.join(" ");
 // console.log(translatedText)
-const translateButton = document.getElementById("translate-button")
+const translateButton = document.getElementById("translate_button")
 
 translateButton.onclick = () => {
-  const translatedArray = inText.value.split(" ").map(translateWord);
-  const translatedText = translatedArray.join(" ");
-  document.getElementById("output").innerText = translatedText;
+    const translatedArray = inText.value.split(" ").map(translateWord);
+    const translatedText = translatedArray.join(" ");
+    document.getElementById("output").innerText = translatedText;
 }
